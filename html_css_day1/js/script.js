39b7cfd79cbba548be04dccb5776796baa502863
anishpdm <anish.vilayil.s@gmail.com>
2018-11-08 16:30:02 +0530

    day 2 final

---
+++ html_css_day1/js/script.js
@@ -0,0 +1,16 @@
+function Read(){
+
+    var name= document.getElementById("getname").value;
+    var age= document.getElementById("getage").value;
+  if(age>=18)
+  {
+      alert("You are Eligible");
+      console.log("Eligible");
+  }
+  else{
+      console.log("Not Eligible");
+  alert(" You are Not Eligible");
+  }
+  
+  }
+  
